29acd91c5dff805528722f39c317b7967ec22dd7
MNivi <146420666+MNivi@users.noreply.github.com>
2023-09-29 12:59:01 +0530

    Add files via upload
    Day1

---
+++ day1/FireFox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class FireFox {
+	public static void main(String[] args) {
+		float browserVersion = 100.2F;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo ='f';
+		System.out.println(browserVersion +" "+browserName +" "+isVisible);
+		System.out.println(releaseYear);
+		System.out.println(browserLogo);
+			
+	}
+
+}
