e56bc5bc98be712b5b728865eed3b9d82f69d952
MNivi <146420666+MNivi@users.noreply.github.com>
2023-09-29 13:00:57 +0530

    Add files via upload
    Day2 and Day3

---
+++ day2/OddNumber.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class OddNumber {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("The Odd numbers are");
+		for (int n=1; n<=20; n++)
+		{
+			if (n%2!=0)
+			{
+				System.out.println(n);
+			}
+		}
+
+	}
+
+}
